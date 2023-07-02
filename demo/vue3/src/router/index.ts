1ad3d06102a9867e7fe1d4d2ac1503caa8efa4b8
Zhlng <1242618701@qq.com>
2023-07-02 19:40:25 +0800

    vue

---
+++ demo/vue3/src/router/index.ts
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Home from'../views/Home.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component:()=>import('../views/Home.vue')
+  },
+  {
+    path:'/login',
+    name:'Login',
+    component:()=>import('../views/LoginRegister.vue')
+  },
+  {
+    path:'/:cachAll(.*)',
+    name:'404',
+    component:()=>import('../views/404.vue')
+  },
+
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
